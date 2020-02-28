641141da3a76a5084311921faf7a84825464f4e1
Alexandr V <alexander.v@smartym.pro>
2020-02-29 01:41:53 +0300

    configured second data source

--- src/main/java/com/amazing/thing/demo/configs/DataConfiguration.java
+++ src/main/java/com/amazing/thing/demo/configs/DataConfiguration.java
@@ -2,15 +2,34 @@ package com.amazing.thing.demo.configs;
 
 import javax.sql.DataSource;
 
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.jdbc.DataSourceBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.jdbc.core.simple.SimpleJdbcInsert;
+import org.springframework.jdbc.core.JdbcTemplate;
 
 @Configuration
 public class DataConfiguration {
 
+    // Source for the users scheme
+
+    @Bean(name = "usersSource")
+    @ConfigurationProperties(prefix = "secondary.datasource")
+    DataSource usersDataSource() {
+        return DataSourceBuilder.create().build();
+    }
+
+    // Source for the main scheme
+    @Bean(name = "mainSource")
+    @ConfigurationProperties(prefix = "primary.datasource")
+    DataSource primaryDataSource() {
+        return DataSourceBuilder.create().build();
+
+    }
+
     @Bean
-    SimpleJdbcInsert simpleJdbcInsert(DataSource dataSource) {
-        return new SimpleJdbcInsert(dataSource);
+    JdbcTemplate jdbcTemplate(@Qualifier("usersSource") DataSource dataSource) {
+        return new JdbcTemplate(dataSource);
     }
 }
